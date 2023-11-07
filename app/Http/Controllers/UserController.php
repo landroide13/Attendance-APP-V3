b3cf942cbdf03182e816e81678ed041edd4332e2
landroide <landrade1313@gmail.com>
2023-11-08 09:46:22 +1300

    filter users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return UserResource::collection(UserModel::all());
+        return UserResource::collection(UserModel::latest()->get());
     }
 
     /**
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -8,6 +8,8 @@ function List( ) {
     const [loading, setLoading] = useState(false);
     const {setNotification} = useStateContext()
 
+    const [option, setOption] = useState('super_admin');
+
     const getUsers = () => {
         setLoading(true)
         axiosClient.get('/users')
@@ -28,32 +30,34 @@ function List( ) {
         getUsers();
       }, []);
 
+  const filtered =  users.filter(user => user.role.name == option)
+  
   return (
     <div className="col-lg-12 col-md-12 col-xl-10 mb-2">
 
-       
             <div className="ul-widget__head pb-20 v-margin">
                 <div className="ul-widget__head-label">
-                    <h3 className="ul-widget__head-title">Users List</h3>
+                    <h3 className="ul-widget__head-title">{ option } List</h3>
                 </div>
-                <button className="btn btn-info dropdown-toggle _r_btn border-0" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All</button>
-                <div className="dropdown-menu" x-placement="bottom-start" style={{ position: 'absolute', top: 0, left: 0 }}><a className="dropdown-item ul-widget__link--font" href="#"> Super Admin</a><a className="dropdown-item ul-widget__link--font" href="#"> Admins</a><a className="dropdown-item ul-widget__link--font" href="#">Tutors</a>
-                    {/* <div className="dropdown-divider"></div><a className="dropdown-item ul-widget__link--font" href="#"><i className="i-Folder-Download"></i> All</a><a className="dropdown-item ul-widget__link--font" href="#"><i className="i-Gears-2"></i> Customize</a> */}
+                <button className="btn btn-info dropdown-toggle _r_btn border-0" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Select</button>
+                <div className="dropdown-menu" x-placement="bottom-start" style={{ position: 'absolute', top: 0, left: 0 }}>
+                  <a className="dropdown-item ul-widget__link--font"  onClick={() => setOption('super_admin')}> Super Admin</a>
+                  <a className="dropdown-item ul-widget__link--font"  onClick={() => setOption('admin')} > Admins</a>
+                  <a className="dropdown-item ul-widget__link--font"  onClick={() => setOption('tutor')}>Tutors</a>
                 </div>
             </div>
             <div className="ul-widget__body">
                 <div className="ul-widget1">
 
-                    { users.map(user => (
+                    {  filtered.map(user => (
                         
                         <div className="ul-widget4__item ul-widget4__users" key={ user.id }>
-                            <div className="ul-widget4__img"><img id="userDropdown" src="../../dist-assets/images/faces/1.jpg" alt="" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" /></div>
+                            <div className="ul-widget4__img"><img id="userDropdown" src="#" alt="" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" /></div>
                             <div className="ul-widget2__info ul-widget4__users-info"><a className="ul-widget2__title" href="#">{ user.first_name } { user.last_name }</a><span className="ul-widget2__username" href="#">{ user.role.name }</span></div><span className="ul-widget4__number t-font-boldest text-success">+500</span>
                         </div>
                         
                       ))
                     }
-                    
                 </div>
             </div>                     
                           
